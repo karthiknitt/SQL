ef318d6a5fdcc882c09b61ba8b3f6af9f617aa26
KARTHIKEYAN_NATARAJAN <karthiknitt@gmail.com>
2022-11-29 09:21:48 +0530

    Create, Insert Delete & Update Tables

--- Create_Tables_MySQL.sql
+++ Create_Tables_MySQL.sql
@@ -1,12 +1,13 @@
+CREATE DATABASE college;
 Use college;
-DROP TABLE student;
+/*--DROP TABLE student;*/
 CREATE TABLE student (
     Id INT PRIMARY KEY AUTO_INCREMENT,
     Name VARCHAR(20) NOT NULL,
     Major VARCHAR(20) DEFAULT 'undecided',
     Rollno INT UNIQUE
 );
-
+/*whaterver */
 DESCRIBE student;
 ALTER TABLE student add Gpa DECIMAL(3,2);
 ALTER TABLE student ADD State VARCHAR(10);
@@ -21,3 +22,28 @@ INSERT INTO student VALUES('','SHRINIVAS','PRE-KG',103,9.5);
 INSERT INTO student(Name,Rollno) VALUES('NATARAJAN',104);
 
 SELECT * FROM student;
+
+UPDATE student
+SET Major = 'ICMAI'
+WHERE Major ='undecided';
+
+SELECT * FROM student;
+
+UPDATE student
+SET Major = 'E&CE'
+WHERE Rollno=101;
+
+SELECT * FROM student;
+
+UPDATE student
+SET Major = 'Engg'
+WHERE Major='Civil' OR Major='E&CE';
+
+SELECT * FROM student;
+
+DELETE FROM student
+WHERE Gpa=NULL;
+
+SELECT * FROM student;
+
+/*DROP DATABASE college;*/
